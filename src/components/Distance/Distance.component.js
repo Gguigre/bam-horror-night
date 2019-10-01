3ae827574a4b4161ef687db0fd095e8c895b3675
Gguigre <gui.gremi@gmail.com>
2019-10-01 12:11:48 +0200

    add approx indication to Distance under actual distance in meters

--- src/components/Distance/Distance.component.js
+++ src/components/Distance/Distance.component.js
@@ -23,7 +23,19 @@ export function Distance({
   ) : !isGeolocationEnabled ? (
     <div>Geolocation is not enabled</div>
   ) : coords ? (
-    <span>Détective ! Vous êtes à {distanceToClue}m de votre indice !</span>
+    <>
+      <span>Détective ! Vous êtes à {distanceToClue}m de votre indice !</span>
+      <br />
+      <span>
+        C'est
+        {distanceToClue > 1000
+          ? " à plus d'un kilomètre "
+          : distanceToClue > 500
+          ? " à quelques centaines de mètres "
+          : " pas très loin "}
+        de votre indice !
+      </span>
+    </>
   ) : (
     <div>Getting the location data</div>
   );
