8a6779112efbcf105beabc0af61fb6cb16e528f1
Gguigre <gui.gremi@gmail.com>
2019-10-29 01:09:17 +0100

    Updates

---
+++ precache-manifest.efe2effc51bb40048b2c49dce31e145d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa04deecb4211f213c35e80ff5037e0",
+    "url": "/bam-horror-night/index.html"
+  },
+  {
+    "revision": "f4b29be66644c92a4bd7",
+    "url": "/bam-horror-night/static/css/main.dd8f71b3.chunk.css"
+  },
+  {
+    "revision": "bb96c6dc42003ad1193d",
+    "url": "/bam-horror-night/static/js/2.e498e329.chunk.js"
+  },
+  {
+    "revision": "f4b29be66644c92a4bd7",
+    "url": "/bam-horror-night/static/js/main.7b67d0e8.chunk.js"
+  },
+  {
+    "revision": "caa462a25dfe83d23b60",
+    "url": "/bam-horror-night/static/js/runtime-main.b324ae96.js"
+  }
+]);
